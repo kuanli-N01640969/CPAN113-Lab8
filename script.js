d297fcf2279b3a30620bb51a49ab6ab8ade1c41a
Kuan Yi Li <kuan_li@outlook.com>
2025-11-23 20:15:34 -0500

    Destrcture object to extract name and courses

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let student = {
     enrolled: true,
     courses: ["CPAN111","CPAN113","CPAN116","CPAN133","CPAN134"],
     info(){
-        console.log("Hello my name is "+this.name,", and my age is",+this.age);
+        console.log("Hello my name is "+this.name,"and my age is "+this.age);
     }
 };
 console.log(student.info());
@@ -15,3 +15,8 @@ console.log(studentJSON);
 //convert back to object
 let parseStudent = JSON.parse(studentJSON);
 console.log(parseStudent);
+//---Part3:Destructuring---
+let {name,courses} = student;
+console.log(name);
+console.log(courses);
+
