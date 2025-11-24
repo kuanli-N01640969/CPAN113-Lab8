90ed33fa8a8d3f9dfa8c756e8214bfb43b630886
Kuan Yi Li <kuan_li@outlook.com>
2025-11-23 19:49:49 -0500

    Created Method info(), to display student information

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ let student = {
     age: 30,
     enrolled: true,
     courses: ["CPAN111","CPAN113","CPAN116","CPAN133","CPAN134"],
-}
+    info(){
+        console.log("Hello my name is "+this.name,", and my age is",+this.age);
+    }
+};
+console.log(student.info());
 
